32e66078dcf4278bc0cebeb28c608308a374c73a
eugenesonickr <eugene.sonic.kr@gmail.com>
2025-07-02 16:54:06 +0900

    Updated Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -47,7 +47,7 @@ const Contact: React.FC = () => {
       info: '서울특별시 용산구 청파로 40',
       detail: '삼구빌딩 10층 (우편번호: 04373)',
       color: 'from-red-500 to-pink-600',
-      action: () => window.open('https://maps.google.com/?q=서울특별시+용산구+청담로+40', '_blank'),
+      action: () => window.open('https://maps.google.com/?q=서울특별시+용산구+청파로+40', '_blank'),
     },
     {
       icon: Phone,
